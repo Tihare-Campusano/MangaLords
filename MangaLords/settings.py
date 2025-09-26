ddd0c3a5010dab3c33f3a1e3d6533de4fa13d2bd
Tihare-Campusano <tb.campusano@gmail.com>
2025-09-25 22:48:52 -0300

    update

--- MangaLords/settings.py
+++ MangaLords/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#_jyey6v#i^)(zrnp2c_=0n4*gk4t7i)qn=34+2%hczwz&i#ds'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
 ALLOWED_HOSTS = []
 
